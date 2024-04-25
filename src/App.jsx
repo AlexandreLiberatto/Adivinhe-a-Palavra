a0a571fbb16dd7958d2b48e753b9e910dc5ee81b
unknown <alexandreliberatto@gmail.com>
2024-04-25 19:00:29 -0300

    Criando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>
+       <h2>Adivinhe a Palavra</h2>
+   </div>
+  )
+}
+
+export default App
---
+++ src/data/words.jsx
@@ -0,0 +1,10 @@
+export const wordsList = {
+    frutas: ['maçã', 'banana', 'laranja', 'uva', 'pera'],
+    transporte: ['carro', 'bicicleta', 'ônibus', 'trem', 'avião'],
+    animais: ['gato', 'cachorro', 'pássaro', 'peixe', 'coelho'],
+    moradia: ['casa', 'apartamento', 'villa', 'cabana', 'tenda'],
+    eletronicos: ['computador', 'celular', 'tablet', 'notebook', 'smartwatch'],
+    esportes: ['futebol', 'basquete', 'tênis', 'futebol de campo', 'vôlei']
+  };
+  
+  
