721f94218215f9ec7500d0948f590186e742f5a7
unknown <alexandreliberatto@gmail.com>
2024-04-26 11:31:06 -0300

    Estilizando a página Game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,13 +1,13 @@
-import './Game.css';
+import "./Game.css";
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div className='game'>
-      <p className='points'>
+    <div className="game">
+      <p className="points">
         <span>Ponução: 000</span>
       </p>
       <h1>Adivinhe a palavra:</h1>
-      <h3 className='tip'>
+      <h3 className="tip">
         Dica sobre a palavra: <span>Dica...</span>
       </h3>
       <div className="wordContainer">
@@ -17,7 +17,7 @@ const Game = ({ verifyLetter }) => {
       <div className="letterContainer">
         <p>Tente adivinhar uma letra da palavra:</p>
         <form>
-          <input type="text" name='letter' maxLength='1' required />
+          <input type="text" name="letter" maxLength="1" required />
           <button>Jogar!</button>
         </form>
       </div>
@@ -27,7 +27,7 @@ const Game = ({ verifyLetter }) => {
         <span>b,</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
